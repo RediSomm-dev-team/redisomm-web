a93365672bbf273e70a650fcc0150a9137729599
Sam <sam@TedPro13.local>
2022-06-03 12:10:33 -0700

    set up linting and style boilerplate

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-}
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')]
+  },
+  reactStrictMode: true
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
